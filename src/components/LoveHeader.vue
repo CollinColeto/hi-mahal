f75b40469b613a228572bde69030cc0a853d8d79
CollinColeto <165641783+CollinColeto@users.noreply.github.com>
2026-08-21 00:44:53 +0800

    Update LoveHeader.vue

--- src/components/LoveHeader.vue
+++ src/components/LoveHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <h1>Hi, My Gorgeous Girl</h1>
     <p>
-      I know things haven’t been perfect lately… and I know I’ve hurt you. This isn’t just a page —
+      I know things didn't go well kanina pero I hope you like what I've made for you. This isn’t just a page,
       it’s me trying to show you how much you mean to me.
     </p>
   </header>
